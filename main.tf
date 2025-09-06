8dd79e6b126617f5754e81b6716cdcbda8e54955
Teju-Aws <tejaswinigundluru@gmail.com>
2025-09-06 17:03:51 +0530

    main.tf

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 resource "aws_instance" "name" {
     ami="ami-0d0ad8bb301edb745"
-    instance_type = "t2.nano"
+    instance_type = "t3.medium"
       #vpc_id = aws_vpc.name.id
       subnet_id = aws_subnet.name.id
       vpc_security_group_ids =[aws_security_group.name.id]
